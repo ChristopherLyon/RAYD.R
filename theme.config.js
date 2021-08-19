1700691c233891b523116fb01965396ae0ef6ddf
Christopher Lyon <31580672+ChristopherLyon@users.noreply.github.com>
2021-08-19 13:42:54 +0200

    Added sources

--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">RAYD.R</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Meteorological Infrastructure Database
+        The Nordic Meteorological Infrastructure Database
       </span>
     </>
   ),
